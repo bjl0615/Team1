9c873fcda9c777c6ca5426208d6557b01f21927e
bjl0615 <bjl0615@gmail.com>
2023-05-09 00:45:42 +0900

    GPS_part1

--- app/src/main/java/com/example/team_project/MapActivity.kt
+++ app/src/main/java/com/example/team_project/MapActivity.kt
@@ -1,9 +1,20 @@
 package com.example.team_project
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.team_project.databinding.ActivityMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,6 +27,40 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding : ActivityMapBinding
     private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ){ permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // fine location 권한이 있다.
+                getCurrentLocation()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // coarse location 권한이 있음
+                getCurrentLocation()
+            }
+            else -> {
+                // TODO 설정으로 보내기 or 교육용 팝업을 띄워서 다시 권한 요청
+            }
+        }
+
+    }
+
+    private val locationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+
+            // 새로 요청된 위치 정보
+            for(location in locationResult.locations) {
+
+                Log.e("MapActivity", "onLocationResult : ${location.latitude} ${location.longitude}")
+
+                location.latitude
+                location.longitude
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +71,47 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+         requestLocationPermission()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    private fun getCurrentLocation() {
+        val locationRequest = LocationRequest
+            .Builder(Priority.PRIORITY_HIGH_ACCURACY , 5 * 1000)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationPermission()
+            return
+        }
+
+        // 권한이 있는 상태
+        fusedLocationClient.requestLocationUpdates(locationRequest , locationCallback , Looper.getMainLooper())
+    }
+
+    private fun requestLocationPermission() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
     override fun onMapReady(map : GoogleMap) {
